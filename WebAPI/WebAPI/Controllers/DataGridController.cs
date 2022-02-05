4ccf6be99bc7b1afa19350e4746f228fd1a171de
Jacob Good <go1446@wayne.edu>
2022-02-05 18:40:37 -0500

    Added base controllers/models for Bar Charts, Data Grids, and KPIs

---
+++ WebAPI/WebAPI/Controllers/DataGridController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataGridController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataGridController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("files/{id:int}")]
+        public async Task<IActionResult> DownloadFile(int id)
+        {
+            var filePath = $"{id}.txt";
+
+            var bytes = await System.IO.File.ReadAllBytesAsync(filePath);
+            return File(bytes, "text/plain", Path.GetFileName(filePath));
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/KPIController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KPIController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public KPIController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("files/{id:int}")]
+        public async Task<IActionResult> DownloadFile(int id)
+        {
+            var filePath = $"{id}.txt";
+
+            var bytes = await System.IO.File.ReadAllBytesAsync(filePath);
+            return File(bytes, "text/plain", Path.GetFileName(filePath));
+        }
+    }
+}
