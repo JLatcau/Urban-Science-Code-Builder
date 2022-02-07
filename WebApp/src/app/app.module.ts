53e2cdb414a6d5c8f3da6035c38d269fbdcb6d8a
Noel Daniel Hall <mrnoeld@gmail.com>
2022-02-06 22:57:32 -0500

    Added DataTable component html

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DatatableComponent } from './datatable/datatable.component';
+import {DataTablesModule} from 'angular-datatables'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatatableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
