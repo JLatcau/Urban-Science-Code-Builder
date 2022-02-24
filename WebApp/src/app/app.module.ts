00f19bed5ce76e390580a5feb2c848a5c637ca92
Sheev Patel <spatel8291@gmail.com>
2022-02-23 19:59:09 -0500

    Moving camera component to pages folder and renaming to camera-page

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
-import { CameraComponent } from './camera/camera.component';
+import { CameraComponent } from './pages/camera-page/camera.component';
 import { WebcamModule } from 'ngx-webcam'
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
