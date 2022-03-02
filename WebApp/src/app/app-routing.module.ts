c281a0beea70aaed3aed82172954a20c4fb51649
Sheev Patel <spatel8291@gmail.com>
2022-03-01 19:27:25 -0500

    Creating empty initial page

--- WebApp/src/app/app-routing.module.ts
+++ WebApp/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ConfirmationPageComponent } from './pages/confirmation-page/confirmatio
 import { DownloadPageComponent } from './pages/download-page/download-page.component';
 import { LoadingPageComponent } from './pages/loading-page/loading-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { OutputPageComponent } from './pages/output-page/output-page.component';
 
 const routes: Routes = [
   {path:'', component: MainPageComponent},
   {path:'loading', component: LoadingPageComponent},
   {path:'camera', component: CameraComponent},
   {path:'download', component: DownloadPageComponent},
-  {path:'confirmation', component: ConfirmationPageComponent}
+  {path:'confirmation', component: ConfirmationPageComponent},
+  {path:'output', component: OutputPageComponent}
 ];
 
 @NgModule({
---
+++ WebApp/src/app/pages/output-page/output-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-output-page',
+  templateUrl: './output-page.component.html',
+  styleUrls: ['./output-page.component.css']
+})
+export class OutputPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
