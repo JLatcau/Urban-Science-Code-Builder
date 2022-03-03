00ef25b69091f7deb9eb680c02ce16dcf3770c32
Jacob Price <ga4116@wayne.edu>
2022-03-03 13:42:04 -0500

    Fixed uploading from file, sends image to confirmation page correctly - JP

--- WebApp/src/app/pages/main-page/main-page.component.ts
+++ WebApp/src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ImageSharingServiceService } from 'src/app/shared/image-sharing-service/image-sharing-service.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  image;
+
+  constructor(private imageService: ImageSharingServiceService,
+    private route: ActivatedRoute,
+    private router: Router) { 
+  }
 
   ngOnInit(): void {
+  }
+
+  onImageSelected(event) {
+    const file:File = event.target.files[0];
+
+    if (file) {
+      this.image = file;
+
+      this.newImage();
+      //console.log(this.image);
+      this.router.navigate(['/confirmation']);
+    }
+  }
+
+  newImage() {
+    this.imageService.newImage(this.image)
   }
 
 }
