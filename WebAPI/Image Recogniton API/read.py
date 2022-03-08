73abe21cf6ac5f2c0c748015f617387ee98da31b
JLatcau <77760532+JLatcau@users.noreply.github.com>
2022-03-08 02:35:58 -0500

    Added bounded boxes, added text output, added organized folders, improved image normalization, Added dynamic file movement

--- WebAPI/Image Recogniton API/read.py
+++ WebAPI/Image Recogniton API/read.py
@@ -1,18 +1,35 @@
 import cv2 as cv
 import numpy as np
-# from pytesseract import pytesseract
 from google.cloud import vision
 import io
 import os
+import shutil
+import glob
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\\Users\\Dess\\Desktop\\keyFile.json"
 client = vision.ImageAnnotatorClient()\
 
 bVar = 'B'
-dVar = 'D'
+gVar = 'G'
 nVar = 'N'
 
+
+bFile = 'DataChartCode/BarChart.html'
+gFile = 'DataChartCode/DataGrid.html'
+nFile = 'DataChartCode/KPI.html'
+
+source_files='Output/*.html'
+target_folder='DataChartCode'
+target_folder2='Output'
+
+# retrieve file list
+filelist=glob.glob(source_files)
+for single_file in filelist:
+     # move file with full paths as shutil.move() parameters
+    shutil.move(single_file,target_folder) 
+
 # # Pulls Image
-img = cv.imread('Photos/B.png')
+img = cv.imread('ImageInput/B.jpg')
 
 # # Resizes Image
 resized = cv.resize(img, (595, 842))
@@ -25,8 +42,11 @@ blank.fill(255) # or img[:] = 255
 gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', resized)
 
+# # Blurs the image
+blur = cv.bilateralFilter(gray,9,75,75)
+
 # # Finds the Edges
-edges = cv.Canny(resized, 125, 175)
+edges = cv.Canny(blur, 125, 175)
 cv.imshow('Edges', edges)
 
 # # Finds the Contours
@@ -34,40 +54,86 @@ contours, hierarchies = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIM
 # # print(f'{len(contours)} contour(s) found!')\
 
 # # Displays detected Contours
-cv.drawContours(blank, contours, -1, (0,0,255), 1)
+cv.drawContours(blank, contours, -1, (0,0,255), 4)
+
+# # Converts back to RGB
+blank = cv.cvtColor(blank, cv.COLOR_BGR2RGB)
+
+# # Creates Bounding Boxes
+for cntr in contours:
+    area = cv.contourArea(cntr)
+    if area > 10 and area < 100:
+        x,y,w,h = cv.boundingRect(cntr)
+        cv.rectangle(blank, (x, y), (x+w, y+h), (0,255,0), 2)
+
 cv.imshow('Contours Drawn', blank)
-cv.imwrite('Photos/cat.jpg', blank)
+cv.imwrite('Output/output.jpg', blank)
 
 
-with io.open('Photos/cat.jpg', 'rb') as image_file:
+with io.open('Output/output.jpg', 'rb') as image_file:
         content = image_file.read()
 
 image = vision.Image(content=content)
 
-response = client.document_text_detection(image=image)
+response = client.text_detection(image=image)
 texts = response.text_annotations
-print('Texts:')
-
-for text in texts:
-    print('\n"{}"'.format(text.description))
-
-    vertices = (['({},{})'.format(vertex.x, vertex.y)
-        for vertex in text.bounding_poly.vertices])
-
-    print('bounds: {}'.format(','.join(vertices)))
 
 if response.error.message:
     raise Exception(
         '{}\nError has occured'.format(
             response.error.message))
 
+with open('Output/output.txt', 'w') as f:
+    f.write('')
+
 for text in texts:
     if bVar==text.description:
         print('Bar Chart')
-    if dVar==text.description:
-        print('Data Sheet')
+        
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+            for vertex in text.bounding_poly.vertices])
+
+        with open('Output/output.txt', 'a') as f:
+            f.write('Texts: \n{}\n'.format(text.description))
+            f.write('Bounds: \n{}\n'.format(','.join(vertices)))
+
+        filelist=glob.glob(bFile)
+        for single_file in filelist:
+        # move file with full paths as shutil.move() parameters
+            shutil.move(single_file,target_folder2) 
+
+    if gVar==text.description:
+        print('Data Grid')
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+            for vertex in text.bounding_poly.vertices])
+
+        with open('Output/output.txt', 'a') as f:
+            f.write('Texts: \n{}\n'.format(text.description))
+            f.write('Bounds: \n{}\n'.format(','.join(vertices)))
+
+        filelist=glob.glob(gFile)
+        for single_file in filelist:
+        # move file with full paths as shutil.move() parameters
+            shutil.move(single_file,target_folder2) 
+
     if nVar==text.description:
         print('KPI')
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+            for vertex in text.bounding_poly.vertices])
+
+        with open('Output/output.txt', 'a') as f:
+            f.write('Texts: \n{}\n'.format(text.description))
+            f.write('Bounds: \n{}\n'.format(','.join(vertices)))
+
+        filelist=glob.glob(nFile)
+        for single_file in filelist:
+        # move file with full paths as shutil.move() parameters
+            shutil.move(single_file,target_folder2) 
+
+    # else:
+    #     print('None Detected')
+    #     with open('output.txt', 'w') as f:
+    #         f.write('None Detected')
 
 
 cv.waitKey(0)
