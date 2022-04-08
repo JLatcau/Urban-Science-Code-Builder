041f38148be5ccae1ab23e18dd703ac823d7094d
Sheev Patel <spatel8291@gmail.com>
2022-04-07 21:45:41 -0400

    Adding more angular animation transitions

--- WebApp/src/app/app.component.ts
+++ WebApp/src/app/app.component.ts
@@ -24,6 +24,42 @@ import { WebcamImage } from 'ngx-webcam';
         group([
           query(':leave', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(100%)'}))]),
           query(':enter', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(0)'}))]),
+        ]),
+      ]),
+      transition('1 => 2', [
+        style({ height: '!'}),
+        query(':enter', style({transform: 'translateX(100%)'})),
+        query(':enter, :leave', style({position: 'absolute', top: '0', left: '0', right: '0'})),
+        group([
+          query(':leave', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(-100%)'}))]),
+          query(':enter', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(0)'}))]),
+        ])
+      ]),
+      transition('2 => 1', [
+        style({ height: '!'}),
+        query(':enter', style({transform: 'translateX(-100%)'})),
+        query(':enter, :leave', style({position: 'absolute', top: '0', left: '0', right: '0'})),
+        group([
+          query(':leave', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(100%)'}))]),
+          query(':enter', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(0)'}))]),
+        ])
+      ]),
+      transition('4 => 5', [
+        style({ height: '!'}),
+        query(':enter', style({transform: 'translateX(100%)'})),
+        query(':enter, :leave', style({position: 'absolute', top: '0', left: '0', right: '0'})),
+        group([
+          query(':leave', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(-100%)'}))]),
+          query(':enter', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(0)'}))]),
+        ])
+      ]),
+      transition('5 => 1', [
+        style({ height: '!'}),
+        query(':enter', style({transform: 'translateX(-100%)'})),
+        query(':enter, :leave', style({position: 'absolute', top: '0', left: '0', right: '0'})),
+        group([
+          query(':leave', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(100%)'}))]),
+          query(':enter', [animate('0.3s cubic-bezier(.35, 0, .25, 1)', style({transform: 'translateX(0)'}))]),
         ])
       ])
     ])
