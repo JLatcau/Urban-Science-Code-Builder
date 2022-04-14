19518a6532126eb8326bde66f68c8f7eabe4a847
Noel Daniel Hall <mrnoeld@gmail.com>
2022-04-14 15:38:51 -0400

    Navigating to Home Page Upon Refresh

--- WebApp/src/app/app.component.ts
+++ WebApp/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { WebcamImage } from 'ngx-webcam';
 import { FileService } from './_service/file.service';
 import { DataService } from './_service/data.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,7 +12,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit,OnDestroy{
-  constructor(private fileService: FileService, private data: DataService) { }
+  constructor(private fileService: FileService, private data: DataService, private router: Router) { }
 
   title = 'WebApp';
   webcamImage: WebcamImage | undefined;
@@ -20,11 +21,12 @@ export class AppComponent implements OnInit,OnDestroy{
 
   ngOnInit(){ 
     this.user_IdSubscription = this.data.currentUser_Id.subscribe(user_id => this.user_id = user_id);
+    this.router.navigate(['']);
   }
 
   @HostListener('window:beforeunload')
   async ngOnDestroy() {
-    var response=await this.fileService.deleteUserData(this.user_id).subscribe();
+    var response=this.fileService.deleteUserData(this.user_id).subscribe();
 
     if (this.user_IdSubscription) {
       this.user_IdSubscription.unsubscribe();
