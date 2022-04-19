eadd37f3288939355148f2af331d2060a7d8bcf5
Jacob Price <ga4116@wayne.edu>
2022-04-18 21:30:53 -0400

    Created a new utility for getTemplateParameterized since the code was redudant in 3 different files (15 or so lines of code). Updated comments for all components, updated schemas, updated package-lock for the version of the schematic (made it 1.1.0 due to amount of changes). Removed debug code as well as commented out functions. -JP

--- WebAPI/Code-Generation-API/code-builder_schematic/code-builder/src/data-grid/index.ts
+++ WebAPI/Code-Generation-API/code-builder_schematic/code-builder/src/data-grid/index.ts
@@ -1,49 +1,21 @@
-import { strings } from '@angular-devkit/core';
-import { Rule, SchematicContext, Tree, apply, url, template, mergeWith, SchematicsException, move, chain } from '@angular-devkit/schematics';
-
-import { parseName } from '@schematics/angular/utility/parse-name'
+import { Rule, SchematicContext, Tree, mergeWith, chain } from '@angular-devkit/schematics';
 import { addComponentToDashboardRule } from '../utils/add-component-to-dashboard-rule';
-import { getCodeInsert } from '../utils/get-code-insert';
+import { getTemplateParameterized } from '../utils/get-template-parameterized';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function dataGrid(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     
-    const workspaceAsBuffer = tree.read('angular.json');
-    if(!workspaceAsBuffer) {
-      throw new SchematicsException('Not an Angular CLI workspace');
-    }
+    // Get the source template with its configurations
+    const sourceTemplateParameterized = getTemplateParameterized(tree, _options);
 
-    const workspace = JSON.parse(workspaceAsBuffer.toString());
-    
-    const projectName = workspace.defaultProject;
-    const project = workspace.projects[projectName];
-
-    const path = `${project.sourceRoot}/${project.projectType === 'application' ? 'app' : 'lib'}`
-
-    const parsed = parseName(path, _options.name);
-
-    _options.name = parsed.name;
-
-    _options.path = parsed.path;
-
-    _options.toAdd = getCodeInsert(_options);
-
-    const sourceTemplate = url(`./files`);
-    const sourceTemplateParametrized = apply(sourceTemplate, [
-      template({
-        ..._options,
-        ...strings
-      }),
-      move(parsed.path)
-    ]);
-
+    // Add the 2 different rules into 1 rule
     const rule = chain([
-      mergeWith(sourceTemplateParametrized),
+      mergeWith(sourceTemplateParameterized),
       addComponentToDashboardRule(_options, tree),
     ]);
 
+    // Create a data-grid component according to the sourceTemplateParameterized
+    // and modify the dashboard.html file to accomodate for the new component
     return rule(tree, _context);
   };
 }
---
+++ WebAPI/Code-Generation-API/code-builder_schematic/code-builder/src/utils/get-template-parameterized.ts
@@ -0,0 +1,44 @@
+import { strings } from '@angular-devkit/core';
+import { Tree, apply, url, template, SchematicsException, move, Source } from '@angular-devkit/schematics';
+
+import { parseName } from '@schematics/angular/utility/parse-name'
+
+export function getTemplateParameterized(tree: Tree, _options: any): Source {
+    // Check if the currenty workspace is an Angular project
+    const workspaceAsBuffer = tree.read('angular.json');
+    if (!workspaceAsBuffer) {
+        throw new SchematicsException('Not an Angular CLI workspace');
+    }
+
+    // Gets the current workspace as a JSON object
+    const workspace = JSON.parse(workspaceAsBuffer.toString());
+
+    const projectName = workspace.defaultProject;
+    const project = workspace.projects[projectName];
+
+    // Returns the path to the app folder of the project if the project type is an application
+    // If not an application, returns the path to the lib folder
+    const path = `${project.sourceRoot}/${project.projectType === 'application' ? 'app' : 'lib'}`
+
+
+    // Reset's the name to the new name in path form after it's been normalized (ex: dashboard -> /dashboard)
+    const parsed = parseName(path, _options.name);
+    _options.name = parsed.name;
+
+    // Sets sourceTemplate equal to the template files
+    const sourceTemplate = url(`./files`);
+
+    // Adds additional configurations to sourceTemplate
+    // Combines sourceTemplate, the additional options provided, 
+    // and the move() function, which will position the newly 
+    // created component properly in the user's project
+    const sourceTemplateParametrized = apply(sourceTemplate, [
+        template({
+            ..._options,
+            ...strings
+        }),
+        move(parsed.path)
+    ]);
+
+    return sourceTemplateParametrized;
+}
